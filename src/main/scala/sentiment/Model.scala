c20050bd6c97d0f558b1c1f12e0700e1754d382d
Paweł Nowak <pawel834@gmail.com>
2015-05-22 13:53:18 +0200

    Refactoring and comments.

--- src/main/scala/sentiment/Model.scala
+++ src/main/scala/sentiment/Model.scala
@@ -16,88 +16,137 @@ import cml.algebra.Constant
 
 object Model {
   import cml.models._
+  import cml.optimization._
   import shapeless.Nat
 
+  /*
+   * First we declare types and implicits that we'll use in the model.
+   */
   type Word[A] = Constant[String, A]
-  implicit val word = Constant.concrete[String]
+  implicit val wordConcrete = Constant.concrete[String]
   implicit val strings = Enumerate.string(Enumerate.char)
   implicit val wordVector = algebra.Vector(Nat(30))
   implicit val wordVectorPair = algebra.Product[wordVector.Type, wordVector.Type]
   implicit val wordTree = algebra.Compose[Tree, Word]
   implicit val wordTreeFunctor = wordTree.functor
   implicit val vectorTree = algebra.Compose[Tree, wordVector.Type]
-  implicit val output = algebra.Vector(Nat(3))
+  implicit val sentimentVec = algebra.Vector(Nat(3))
 
-  val model: cml.Model[wordTree.Type, output.Type] = Chain4(
+  /**
+   * A recurisve neural network model.
+   */
+  val model: cml.Model[wordTree.Type, sentimentVec.Type] = Chain4(
+    // Firstly map words to word vectors.
     FunctorMap[Tree, Word, wordVector.Type](
       Function[String, wordVector.Type]
     ) : cml.Model[wordTree.Type, vectorTree.Type],
+    // Now fold the tree using matrix multiplication with bias (an affine map) and sigmoid activation.
     Reduce[Tree, wordVector.Type](Chain2(
       AffineMap[wordVectorPair.Type, wordVector.Type],
       Pointwise[wordVector.Type](AnalyticMap.sigmoid)
     )) : cml.Model[vectorTree.Type, wordVector.Type],
-    AffineMap[wordVector.Type, output.Type],
-    Softmax[output.Type]
+    // The next layer maps the word vector to a sentiment vector.
+    AffineMap[wordVector.Type, sentimentVec.Type],
+    // Finally we apply softmax to get probabilities.
+    Softmax[sentimentVec.Type]
   )
 
-  val costFun = new CostFun[wordTree.Type, output.Type] {
-    override def scoreSample[A](sample: ScoredSample[wordTree.Type[A], output.Type[A]])
-        (implicit an: Analytic[A]): A = {
+  /**
+   * The cost function for our model.
+   */
+  val costFun = new CostFun[wordTree.Type, sentimentVec.Type] {
+    /**
+     * This function scores a single sample (input, expected output and actual output triple).
+     *
+     * The cost for the whole data set is assumed to be the mean of scores for each sample.
+     */
+    override def scoreSample[A](sample: Sample[wordTree.Type[A], sentimentVec.Type[A]])(implicit an: Analytic[A]): A = {
       import an.analyticSyntax._
       val eps = fromDouble(0.0001)
-      val j = ^(sample.expected, sample.actual){ case (e, a) => {
+
+      // Softmax regression cost function. We add epsilon to prevent taking the log of 0.
+      val j = ^(sample.expected, sample.actual){ case (e, a) =>
         e * (a + eps).log
-      }}
-      -output.sum(j)
+      }
+      -sentimentVec.sum(j)
     }
 
-    override def regularization[V[_], A](instance: V[A])(implicit an: Analytic[A], space: LocallyConcrete[V]): A = {
-      import an.analyticSyntax._
-      fromDouble(0.01) * space.quadrance(instance)
-    }
+    /**
+     * Computes the regularization term for a model instance.
+     */
+    override def regularization[V[_], A](instance: V[A])(implicit an: Analytic[A], space: LocallyConcrete[V]): A =
+      an.mul(an.fromDouble(0.01), space.quadrance(instance))
   }
+
+  /**
+   * Now we create an optimizer that will train our model. The MultiOpt optimizer is a higher-order optimizer that
+   * launches multiple optimizers in parallel and collects their results. We use gradient descent as our base optimizer.
+   *
+   * Gradient descent takes an optional gradient transformer, which is a function applied to the gradient before a
+   * step is made. Here we apply numerical stabilization and then AdaGrad to automatically take care of step size.
+   */
+  val optimizer = MultiOpt(
+    populationSize = 4,
+    optimizer = GradientDescent(
+      model,
+      iterations = 200,
+      gradTrans = Stabilize.andThen(AdaGrad)
+    )
+  )
+
+  /**
+   * We need to declare what automatic differentiation engine should be used. Backpropagation is the best.
+   */
+  implicit val diffEngine = ad.Backward
 }
 
-case class Model (
-  instance: Model.model.Type[Double]
+/**
+ * Wraps a model instance.
+ */
+case class ModelInstance (
+  get: Model.model.Type[Double]
 )
 
-class Algorithm extends P2LAlgorithm[PreparedData, Model, Query, PredictedResult] {
+class Algorithm extends P2LAlgorithm[PreparedData, ModelInstance, Query, PredictedResult] {
 
   @transient lazy val logger = Logger[this.type]
 
-  def train(sc: SparkContext, data: PreparedData): Model = {
-    import cml.optimization._
+  /**
+   * Trains a model instance.
+   */
+  def train(sc: SparkContext, data: PreparedData): ModelInstance = {
     import Model._
 
-    // Convert the data to our input format.
+    // First we have to convert the data set to our model's input format.
     val dataSet = data.sentences.map { case (tree, expected) => {
       val index = sentiments(expected.sentiment)
       val in: wordTree.Type[Double] = tree.map(Constant(_))
-      val out: output.Type[Double] = output.tabulateLC(Map(index -> expected.confidence))
+      val out: sentimentVec.Type[Double] = sentimentVec.tabulateLC(Map(index -> expected.confidence))
       (in, out)
     }}
 
-    val optimizer = MultiOpt(
-      populationSize = 4,
-      optimizer = GradientDescent(
-        model,
-        iterations = 200,
-        gradTrans = Stabilize.andThen(AdaGrad)
-      )
-    )
-
+    // Value that the new model instances will be filled with.
     val rng = new Random()
-    implicit val diffEngine = ad.Backward
+    val filler = () => rng.nextDouble * 4d - 2d
 
     val startTime = System.currentTimeMillis()
-    val inst = optimizer[Double](Vector(), dataSet, costFun, rng.nextDouble * 4d - 2d)
-      .minBy(_._1)
-      ._2
+    // Run the optimizer!
+    val inst =
+      optimizer[Double](
+        // This is the starting population, in case we want to improve existing instances.
+        // We do not have any trained model instances so we just pass an empty vector.
+        population = Vector(),
+        data = dataSet,
+        costFun = costFun,
+        default = filler())
+      // Optimizer returns a vector of (cost, instance) pairs. Here we select the instance with the lowest cost.
+      .minBy(_._1)._2
+      // And unfortunetly we have to explicitely cast it to the right type. This is because scala doesn't know
+      // that model.Type = optimizer.model.Type, even thought it is quite obvious to us since model == optimizer.model.
       .asInstanceOf[model.Type[Double]]
-
     val endTime = System.currentTimeMillis()
 
+    // Print the predictions on the training set for testing.
     for (t <- dataSet) {
       val sentence = t._1.map(_.value).foldl1(x => y => x + " " + y)
       val value = model(inst)(t._1)
@@ -106,19 +155,27 @@ class Algorithm extends P2LAlgorithm[PreparedData, Model, Query, PredictedResult
         sentiment = sentiments.inverse(prediction._2),
         confidence = prediction._1
       )
-      println(s"${sentence} -> ${res}")
+      println(s"$sentence -> $res")
     }
-
     println(s"Time: ${endTime - startTime}ms")
-    Model(inst)
+
+    ModelInstance(inst)
   }
 
-  def predict(model: Model, query: Query): PredictedResult = {
-    import Model.output.traverseSyntax._
+  /**
+   * Queries the model.
+   */
+  def predict(modelInstance: ModelInstance, query: Query): PredictedResult = {
+    import Model._
+    import sentimentVec.traverseSyntax._
 
+    // Parse the sentence into a tree.
     val tree = Parser(query.sentence)
-    val value = Model.model(model.instance)(tree.map(Constant(_)))
 
+    // Apply the model.
+    val value = model(modelInstance.get)(tree.map(Constant(_)))
+
+    // Extract the class with highest confidence.
     val prediction = value.toList.zipWithIndex.maxBy(_._1)
     PredictedResult(
       sentiment = sentiments.inverse(prediction._2),
@@ -126,5 +183,8 @@ class Algorithm extends P2LAlgorithm[PreparedData, Model, Query, PredictedResult
     )
   }
 
+  /**
+   * A mapping between sentiment vector indices and sentiment labels.
+   */
   val sentiments = BiMap.stringInt(Array("No", "Yes", "NA"))
 }
